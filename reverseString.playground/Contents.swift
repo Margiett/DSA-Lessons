662c2c141f3d3b405380a720551f5657f91e773d
Margiett Gil <mxg627@icloud.com>
2020-05-25 23:55:49 -0400

    did the reverse string using a forloop

--- reverseString.playground/Contents.swift
+++ reverseString.playground/Contents.swift
@@ -4,3 +4,27 @@ var aString = "Hello"
 print(aString)
 var reverse = String(aString.reversed())
 print(reverse)
+
+
+func string(aStr: String) -> String {
+    var str = "" // this is an empty string
+    //    print(str)
+    //
+    for character in aStr {
+       // character is a the letter which is adding it to the empty string
+        // hello
+        // h + ""
+        
+        print(character)
+      
+        print(aStr)
+      
+        str = "\(character)" + str 
+        print(str)
+    }
+    return str
+    
+}
+print(string(aStr: "Hello"))
+
+
